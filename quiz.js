dfb30a5a3e57a39bd6afc565922c70f2ff1a2356
Mike95721 <93540527+Mike95721@users.noreply.github.com>
2021-11-14 19:19:36 +0000

    init

---
+++ quiz.js
@@ -0,0 +1,39 @@
+type="text/javascript">
+   function result() {  
+     
+
+  var score=0;
+      (document.getElementById('correct1').checked) 
+     {
+         score++;
+  
+     }
+     if (document.getElementById('correct2').checked) 
+     {
+       score++;
+     }
+  
+     if (document.getElementById('correct3').checked) 
+     {
+       score++;
+     } 
+  
+     if (document.getElementById('correct4').checked) 
+     {
+       score++;
+     } 
+  
+     if (document.getElementById('correct5').checked) 
+     {
+       score++;
+     } 
+     
+     document.write("Your score is:"+score);
+   }
+
+   
+
+   
+   
+
+  
